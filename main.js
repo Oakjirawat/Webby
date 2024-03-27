bc4a039773c662ba67b91d2b9dd8412db35c9a3d
Oakjirawat <147781863+Oakjirawat@users.noreply.github.com>
2024-03-27 11:07:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+//var keys= prompt("Enter acess key");
+var pubilck="da>`iHJ?jyaP`)Q8%=ITQ#;V4WCP0hsWjIIoJJ38r_hCheIAK*(B*'XULg8na|*";
+var private="KNh1|%a7$P4ms0)JnH'zf:(=&kZx7q";
+var emear="X9U6nkTtdd"
+var beta="guest"
+/*if(!(keys=="" || keys==null)){
+    if(!(keys==pubilck || keys==private || keys==emear || keys==beta)){
+       
+        console.log("invaild key");
+    }
+    console.log("key detect");
+}
+else{
+   while(1){
+    window.alert("worng key")
+   }
+}*/
+
+
+console.log(keys);
+
